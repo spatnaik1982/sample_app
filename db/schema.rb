2d0d9489a219f9b59e2a8d8a8d5a174174ad153f
Sarthak Patnaik <spatnaik1982@gmail.com>
2013-02-08 23:42:21 +0530

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208085300) do
+ActiveRecord::Schema.define(:version => 20130208172320) do
 
   create_table "users", :force => true do |t|
     t.string   "firstname"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(:version => 20130208085300) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
